74f803d06d05ef03bb535272c4fe26e9a13ee448
Fulya34 <funda.sisek_49@icloud.com>
2026-01-18 14:44:33 +0300

    proje yapıldı

---
+++ src/main/java/com/workintech/fswebs17d1/controller/AnimalController.java
@@ -0,0 +1,52 @@
+package com.workintech.fswebs17d1.controller;
+import org.springframework.beans.factory.annotation.Value;
+import com.workintech.fswebs17d1.entity.Animal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@RestController
+@RequestMapping("/workintech/animal")
+public class AnimalController {
+
+    private Map<Integer, Animal> animals = new HashMap<>();
+
+    // [GET] tüm hayvanları getir
+    @GetMapping
+    public List<Animal> getAllAnimals() {
+        return new ArrayList<>(animals.values());
+    }
+
+    // [GET] id'ye göre hayvan getir
+    @GetMapping("/{id}")
+    public Animal getAnimalById(@PathVariable Integer id) {
+        return animals.get(id);
+    }
+
+    // [POST] yeni hayvan ekle
+    @PostMapping
+    public Animal addAnimal(@RequestBody Animal animal) {
+        animals.put(animal.getId(), animal);
+        return animal;
+    }
+
+    // [PUT] hayvan güncelle
+    @PutMapping("/{id}")
+    public Animal updateAnimal(@PathVariable Integer id,
+                               @RequestBody Animal animal) {
+        animals.put(id, animal);
+        return animal;
+    }
+
+    // [DELETE] hayvan sil
+    @DeleteMapping("/{id}")
+    public String deleteAnimal(@PathVariable Integer id) {
+        animals.remove(id);
+        return "Animal deleted: " + id;
+    }
+    @Value("${course.name}")
+    private String courseName;
+
+    @Value("${project.developer.fullname}")
+    private String developerName;
+}
